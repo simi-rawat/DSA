85ffc546d3367c711be3583c219ebdb998f9d5b9
Simran Rawat <146023943+simi-rawat@users.noreply.github.com>
2026-08-10 09:53:31 +0530

    Time: 31 ms (81.44%) | Memory: 8.9 MB (90.65%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -4,17 +4,12 @@ public:
 
         vector<bool> dp(n + 1, false);
 
-        // dp[0] = false
-        // No stones -> player cannot make a move -> loses
-
         for(int i = 1; i <= n; i++) {
 
             for(int j = 1; j * j <= i; j++) {
 
                 int square = j * j;
 
-                // If opponent is in a losing state,
-                // current player wins.
                 if(dp[i - square] == false) {
                     dp[i] = true;
                     break;
