81f95e5202d169dbec4ad05908208835c6e9335e
Simran Rawat <146023943+simi-rawat@users.noreply.github.com>
2026-08-18 12:29:12 +0530

    Time: 6 ms (37.59%) | Memory: 31.8 MB (34.96%) - LeetSync

---
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+
+        int n = nums.size();
+
+        int count[51] = {0};
+        for(int i = 0; i <= n - k; i++) {
+
+            set<int> st;
+
+            for(int j = i; j < i + k; j++) {
+                st.insert(nums[j]);
+            }
+            for(int x : st) {
+                count[x]++;
+            }
+        }
+
+        for(int x = 50; x >= 0; x--) {
+            if(count[x] == 1)
+                return x;
+        }
+
+        return -1;
+    }
+};
