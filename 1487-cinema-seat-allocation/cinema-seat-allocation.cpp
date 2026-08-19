d184b3e4bf300afbe2fb62938b798cddc1267d63
Simran Rawat <146023943+simi-rawat@users.noreply.github.com>
2026-08-19 14:37:45 +0530

    Time: 21 ms (74.83%) | Memory: 60.1 MB (73.48%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -9,8 +9,6 @@ public:
         }
 
         long long ans = 2LL * n;
-
-        // Masks for the three possible blocks
         int left   = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5);
         int middle = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
         int right  = (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9);
@@ -19,17 +17,14 @@ public:
 
             int mask = p.second;
 
-            // We initially counted this row as 2 groups
             ans -= 2;
 
             if((mask & left) == 0 && (mask & right) == 0) {
-                // Both outer blocks available
                 ans += 2;
             }
             else if((mask & left) == 0 ||
                     (mask & middle) == 0 ||
                     (mask & right) == 0) {
-                // At least one block available
                 ans += 1;
             }
         }
